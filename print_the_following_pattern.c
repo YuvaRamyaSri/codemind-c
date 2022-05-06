693f0f55c519955b5fede33d265d83439ac0ee9b
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-05-06 19:29:12 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || j==1 ||j==n)             {                 printf("* ");             }             else             {                 printf("  ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -7,13 +7,13 @@ int main()
     {
         for(j=1;j<=n;j++)
         {
-            if(i==j || (i+j)==n+1)
+            if(i==j || j==1 ||j==n)
             {
-                printf("x");
+                printf("* ");
             }
             else
             {
-                printf("0");
+                printf("  ");
             }
         }
         printf("
