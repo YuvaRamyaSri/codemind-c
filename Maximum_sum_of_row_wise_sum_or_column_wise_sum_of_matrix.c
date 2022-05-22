4efd39b9abb7149c00c20e498bfe357da9bfb101
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-05-23 00:09:46 +0530

    #include<stdio.h> int main() {     int n,m,a[100][100],i,j,s,max=0,maxi=0,c=0;     scanf("%d %d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<m;j++)     {         s=0;         for(i=0;i<n;i++)         {             s=s+a[i][j];         }         if(max<s)         {             max=s;         }     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<m;j++)         {             c=c+a[i][j];         }         if(maxi<c)         {             maxi=c;         }     }     if(max>maxi)     {         printf("%d",max);     }     else     {         printf("%d",maxi);     } }

---
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,a[100][100],i,j,s,max=0,maxi=0,c=0;
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(j=0;j<m;j++)
+    {
+        s=0;
+        for(i=0;i<n;i++)
+        {
+            s=s+a[i][j];
+        }
+        if(max<s)
+        {
+            max=s;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<m;j++)
+        {
+            c=c+a[i][j];
+        }
+        if(maxi<c)
+        {
+            maxi=c;
+        }
+    }
+    if(max>maxi)
+    {
+        printf("%d",max);
+    }
+    else
+    {
+        printf("%d",maxi);
+    }
+}
