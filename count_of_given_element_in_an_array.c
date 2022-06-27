6620ef448b97360902443c4701942868633ea336
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-06-27 23:15:30 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,m=0,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]==k)         {             m++;         }     }     printf("%d",m); }

---
+++ count_of_given_element_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[1000],i,m=0,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==k)
+        {
+            m++;
+        }
+    }
+    printf("%d",m);
+}
