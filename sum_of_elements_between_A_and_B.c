53f9e99a4a207f09fced60f4f750e0388efe8f2d
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-06-28 10:19:39 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,a,b,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))         {             c=c+arr[i];         }     }     printf("%d",c); }

--- sum_of_elements_between_A_and_B.c
+++ sum_of_elements_between_A_and_B.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],a,b,i,c=0,min=1000;
+    int n,arr[1000],i,a,b,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     for(i=0;i<n;i++)
     {
         if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))
         {
-           c=c+arr[i];
+            c=c+arr[i];
         }
     }
     printf("%d",c);
