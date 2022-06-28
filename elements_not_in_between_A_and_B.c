bbf164b79276fc29e075ef1a168e608e3f4264e5
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-06-28 09:57:22 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,a,b,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a||arr[i]>b)         {             printf("%d ",arr[i]);             c++;         }     }     if(c==0)     {         printf("-1");     } }

--- elements_not_in_between_A_and_B.c
+++ elements_not_in_between_A_and_B.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],a,b,i,c=0;
+    int n,arr[1000],i,a,b,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     for(i=0;i<n;i++)
     {
-        if((arr[i]<a&&arr[i]<b)||(arr[i]>a&&arr[i]>b))
+        if(arr[i]<a||arr[i]>b)
         {
             printf("%d ",arr[i]);
             c++;
@@ -19,6 +19,5 @@ int main()
     if(c==0)
     {
         printf("-1");
-        
     }
 }
