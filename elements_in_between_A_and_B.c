8de7047f56c2ccbab730e92621567335742db8f1
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-06-28 10:06:47 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,a,b,m=100,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))         {             c=1;             printf("%d ",arr[i]);         }     }     if(c==0)     {         printf("-1");     } }

--- elements_in_between_A_and_B.c
+++ elements_in_between_A_and_B.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],a,b,i,c=0,min=1000;
+    int n,arr[1000],i,a,b,m=100,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     for(i=0;i<n;i++)
     {
         if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))
         {
-           printf("%d ",arr[i]);
-           c++;
+            c=1;
+            printf("%d ",arr[i]);
         }
     }
     if(c==0)
     {
-    printf("-1");
+        printf("-1");
     }
 }
