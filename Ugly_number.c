db973dcdc75e9bad6379dca9d9efd69dbd8bd23c
YuvaRamyaSri <97425224+YuvaRamyaSri@users.noreply.github.com>
2022-06-29 11:03:25 +0530

    # include <stdio.h> # include <string.h>
    int main()
    {
    int n,x=0;
      scanf("%d",&n);
    
          if (n <= 0) {  
            }
           while (n != 1) 
           {  
                if (n % 5 == 0) 
                {  
                    n /= 5;  
                } 
                else if (n % 3 == 0) 
                {  
                    n /= 3;  
                } 
                else if (n % 2 == 0) 
                {  
                    n /= 2;  
                } 
                else 
                {  
                    printf("Not Ugly Number"); 
                    x = 1;  
                    break;  
                }  
            } 
    		        if (x==0)
    		        { 
                    printf("Ugly Number");
                    }
    }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,39 +1,36 @@
-#include<stdio.h>
-int isugly(int n)
-{
-    if(n==1)
-    {
-        return 1;
-    }
-    if(n<=0)
-     {
-         return 0;
-     }
-     if(n%2==0)
-     {
-         return (isugly(n/2));
-     }
-     if(n%3==0)
-     {
-         return (isugly(n/3));
-     }
-     if(n%5==0)
-     {
-         return (isugly(n/5));
-     }
-     return 0;
-}
+# include <stdio.h>
+# include <string.h>
+
 int main()
 {
-    int n,k;
-    scanf("%d",&n);
-    k=isugly(n);
-    if(k==1)
-    {
-        printf("Ugly Number");
-    }
-    else
-    {
-        printf("Not Ugly Number");
-    }
+int n,x=0;
+  scanf("%d",&n);
+
+      if (n <= 0) {  
+        }
+       while (n != 1) 
+       {  
+            if (n % 5 == 0) 
+            {  
+                n /= 5;  
+            } 
+            else if (n % 3 == 0) 
+            {  
+                n /= 3;  
+            } 
+            else if (n % 2 == 0) 
+            {  
+                n /= 2;  
+            } 
+            else 
+            {  
+                printf("Not Ugly Number"); 
+                x = 1;  
+                break;  
+            }  
+        } 
+		        if (x==0)
+		        { 
+                printf("Ugly Number");
+                }
 }
